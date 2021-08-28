3f579f820fcd319cd681fb356e5480788cd07da2
Zoe <dev.mennaalaa@gmail.com>
2021-08-28 11:38:24 +0200

    fixing mobile view

--- js/app.js
+++ js/app.js
@@ -28,13 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
   //function that checks if the current section's  in the viewPort
   const isInViewport = (element) => {
     const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    console.log("element =", element);
+    console.log("rect =", rect);
+    return rect.top >= 0 && rect.top < rect.height;
   };
 
   /**
