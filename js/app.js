f3481c1c23ef26ce0b9e43f8951401d70d4d86d9
devMenna <dev.mennaalaa@gmail.com>
2023-03-25 20:32:56 +0200

    testing

--- js/app.js
+++ js/app.js
@@ -1,45 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   //function that checks if the current section's  in the viewPort
   const isInViewport = (element) => {
     const rect = element.getBoundingClientRect();
-    return rect.top >= 0 && rect.top < rect.height;
   };
 
-  const sections = document.querySelectorAll("section");
+  const sections = document.querySelectorAll('section');
 
   const numOfSections = sections.length;
   //loop to create list items and adding a elements to it then sets an attribute with the sections link to scroll over there by click
 
   for (let i = 1; i <= numOfSections; i++) {
-    let navBarLink = document.createElement("a");
-    let navBarListItem = document.createElement("LI");
-    navBarLink.textContent = "Section " + i;
-    navBarLink.setAttribute("href", "index.html#section" + i);
+    let navBarLink = document.createElement('a');
+    let navBarListItem = document.createElement('LI');
+    navBarLink.textContent = 'Section ' + i;
+    navBarLink.setAttribute('href', 'index.html#section' + i);
     navBarListItem.appendChild(navBarLink);
-    navBarLink.classList.add("menu__link");
-    document.getElementById("navbar__list").appendChild(navBarListItem);
+    navBarLink.classList.add('menu__link');
+    document.getElementById('navbar__list').appendChild(navBarListItem);
 
     //eventListener to scroll to the section in view when clicked
 
-    navBarLink.addEventListener("click", (event) => {
+    navBarLink.addEventListener('click', (event) => {
       event.preventDefault();
 
       document
-        .getElementById("section" + i)
-        .scrollIntoView({ behavior: "smooth" });
+        .getElementById('section' + i)
+        .scrollIntoView({ behavior: 'smooth' });
     });
   }
 
   //eventListener that waits for the scroll to happen, calls the function to check if section in view , adds class therefore adding style to it
-  document.addEventListener("scroll", () => {
-    const navbarElements = document.querySelectorAll("li");
+  document.addEventListener('scroll', () => {
+    const navbarElements = document.querySelectorAll('li');
     sections.forEach((el, idx) => {
       if (isInViewport(el)) {
-        el.classList.add("your-active-class");
-        navbarElements[idx].classList.add("in-view");
+        el.classList.add('your-active-class');
+        navbarElements[idx].classList.add('in-view');
       } else {
-        el.classList.remove("your-active-class");
-        navbarElements[idx].classList.remove("in-view");
+        el.classList.remove('your-active-class');
+        navbarElements[idx].classList.remove('in-view');
       }
     });
   });
